a5bd7daf628a6da198cd804f079702c473ec8559
tommy mok <tommymok@tommyde-MacBook-Pro.local>
2017-03-19 23:36:00 +0800

    lab1
    first

---
+++ lab1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int main()
+{
+  cin >> n;
+  cout << n << " ";
+   while(n!=1)
+   {
+     if(n%2==0)
+     { n=n/2;
+         cout << n << " ";
+     }
+     else
+     { n=3*n+1;
+         cout << n << " ";
+     }
+ }
+}
